8ffbab64e50e780c3c450d1aad61d083dc1863de
memellisa <mellisa5@connect.hku.hk>
2020-05-01 14:34:01 +0700

    Updated the function main for some formatting and change of error messages

--- Makefile
+++ Makefile
@@ -9,16 +9,18 @@ help.o: help.cpp help.h
 start.o: start.cpp start.h help.o
 	g++ $(FLAGS) -c $<
 
+score_calculator.o: score_calculator.cpp score_calculator.h
+
 check_word.o: check_word.cpp check_word.h
 	g++ $(FLAGS) -c $<
 
 print_board.o: print_board.cpp print_board.h
 	g++ $(FLAGS) -c $<
 
-word_solitaire_main.o: word_solitaire_main.cpp random_letter.h check_word.h
+word_solitaire_main.o: word_solitaire_main.cpp random_letter.h check_word.h score_calculator.h
 	g++ $(FLAGS) -c $<
 
-word_solitaire: random_letter.o word_solitaire_main.o print_board.o start.o help.o check_word.o
+word_solitaire: random_letter.o word_solitaire_main.o print_board.o start.o help.o check_word.o score_calculator.o
 	g++ $(FLAGS) $^ -o $@
 
 clean:
--- word_solitaire_main.cpp
+++ word_solitaire_main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include "random_letter.h"
 #include "print_board.h"
 #include "start.h"
 #include "help.h"
 #include "check_word.h"
+#include "score_calculator.h"
 using namespace std;
 
 
@@ -16,6 +18,7 @@ int main(){
     string input;
     int row_number;
     bool valid;
+    int word_length;
     row_1.assign(10, '_');
     row_2.assign(10, '_');
     row_3.assign(10, '_');
@@ -23,13 +26,13 @@ int main(){
     row_5.assign(10, '_');
     letter = random_letter();
     start();
-    cout << ">> Happy playing! <<" << endl;
+    cout << right;
+    cout << setw(42) << ">> Happy playing! <<" << endl;
     print_board(row_1, row_2, row_3, row_4, row_5, score, mistake);
 
     while (input != "exit"){
         cout << "Letter: " << letter << endl;
-        cout << "Enter Command" << endl;
-        cout << "Your input: ";
+        cout << "Enter Command: ";
         cin >> input;
         if (input == "1"){
             if (counter_1 < 10){
@@ -37,8 +40,8 @@ int main(){
                 ++counter_1;
                 print_board(row_1, row_2, row_3, row_4, row_5, score, mistake);
             } else {
-                cout << "Sorry. Your input is invalid! The row is full." << endl;
-                cout << "Please enter another row or submit";
+                cout << endl << "Sorry. The row is full!" << endl << endl;
+                continue;
             }
         }
         else if (input == "2"){
@@ -47,8 +50,8 @@ int main(){
                 ++counter_2;
                 print_board(row_1, row_2, row_3, row_4, row_5, score, mistake);
             } else {
-                cout << "Sorry. Your input is invalid! The row is full." << endl;
-                cout << "Please enter another row or submit";
+                cout << endl << "Sorry. The row is full!" << endl << endl;
+                continue;
             }
         }
         else if (input == "3"){
@@ -57,8 +60,8 @@ int main(){
                 ++counter_3;
                 print_board(row_1, row_2, row_3, row_4, row_5, score, mistake);
             } else {
-                cout << "Sorry. Your input is invalid! The row is full." << endl;
-                cout << "Please enter another row or submit";
+                cout << endl << "Sorry. The row is full!" << endl << endl;
+                continue;
             }
         }
         else if (input == "4"){
@@ -67,8 +70,8 @@ int main(){
                 ++counter_4;
                 print_board(row_1, row_2, row_3, row_4, row_5, score, mistake);
             } else {
-                cout << "Sorry. Your input is invalid! The row is full." << endl;
-                cout << "Please enter another row or submit";
+                cout << endl << "Sorry. The row is full!" << endl << endl;
+                continue;
             }
         }
         else if (input == "5"){
@@ -77,40 +80,67 @@ int main(){
                 ++counter_5;
                 print_board(row_1, row_2, row_3, row_4, row_5, score, mistake);
             } else {
-                cout << "Sorry. Your input is invalid! The row is full." << endl;
-                cout << "Please enter another row or submit";
+                cout << endl << "Sorry. The row is full!" << endl << endl;
+                continue;
             }
         }
         else if (input == "exit"){
             break;
         }
         else if (input == "submit"){
-            cout << "Which row would you like to submit?" << endl;
+            cout << endl << "Which row would you like to submit? ";
             cin >> row_number;
+            cout << endl;
 
             if (row_number == 1){
+                if (counter_1 == 0){
+                    cout << "Sorry. The row you are submitting is empty!" << endl;
+                    continue;
+                }
                 valid = check_word (row_1, counter_1);
+                word_length = counter_1;
                 counter_1 = 0;
                 row_1.assign(10, '_');
             } else if (row_number == 2){
+                if (counter_2 == 0){
+                    cout << "Sorry. The row you are submitting is empty!" << endl;
+                    continue;
+                }
                 valid = check_word (row_2, counter_2);
+                word_length = counter_2;
                 counter_2 = 0;
                 row_2.assign(10, '_');
             } else if (row_number == 3){
+                if (counter_3 == 0){
+                    cout << "Sorry. The row you are submitting is empty!" << endl;
+                    continue;
+                }
                 valid = check_word(row_3, counter_3);
+                word_length = counter_3;
                 counter_3 = 0;
                 row_3.assign(10, '_');
             } else if (row_number == 4){
+                if (counter_4 == 0){
+                    cout << "Sorry. The row you are submitting is empty!" << endl;
+                    continue;
+                }
                 valid = check_word(row_4, counter_4);
+                word_length = counter_4;
                 counter_4 = 0;
                 row_4.assign(10, '_');
             } else if (row_number == 5){
+                if (counter_5 == 0){
+                    cout << "Sorry. The row you are submitting is empty!" << endl << endl;
+                    continue;
+                }
                 valid = check_word(row_5, counter_5);
+                word_length = counter_5;
                 counter_5 = 0;
                 row_5.assign(10, '_');
             } 
 
             if (valid){
+                score_calculator(word_length, score);
                 cout << "YEAY YOU DID IT!!!" << endl;
                 
             } else{
@@ -125,7 +155,7 @@ int main(){
         }
         else{
             cout << endl;
-            cout << "Sorry. Your input is invalid!" << endl;
+            cout << "Sorry. Your input is invalid!" << endl << endl;
             continue;
         }
         letter = random_letter();
