c21d6decbc11bcefc6b39675d35794b17c4ff95a
memellisa <mellisa5@connect.hku.hk>
2020-05-06 15:26:49 +0700

    Updated save_progress.h

--- save_progress.h
+++ save_progress.h
@@ -1,6 +1,6 @@
 #ifndef SAVE_PROGRESS_H
 #define SAVE_PROGRESS_H
 
-void save_progress();
+void save_progress(int score, int mistake, char letter, char * row_1, char * row_2, char * row_3, char * row_4, char * row_5);
 
 #endif
